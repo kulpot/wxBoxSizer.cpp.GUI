f6c93375bd72ddfe5061454fa9b597e4e4516b92
Kulpot <kulpotwahu@gmail.com>
2024-05-11 05:13:52 +0800

    App.cpp->
     mainFrame->SetClientSize(1000, 600);

--- wxBoxSizer/App.cpp
+++ wxBoxSizer/App.cpp
@@ -7,6 +7,7 @@ wxIMPLEMENT_APP(App);
 bool App::OnInit() 
 {
 	MainFrame* mainFrame = new MainFrame("C++ GUI");
+	mainFrame->SetClientSize(1000, 600);
 	mainFrame->Center();
 	mainFrame->Show();
 	return true;
