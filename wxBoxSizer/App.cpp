90f232ba8c6a19f4e5480c3b5844eabaeeb9fbce
Kulpot <kulpotwahu@gmail.com>
2024-05-11 05:11:46 +0800

    App.cpp-> #include "App.h" #include "MainFrame.h" #include <wx/wx.h>
    wxIMPLEMENT_APP(App);
    
    bool App::OnInit()
    {
     MainFrame* mainFrame = new MainFrame("C++ GUI");
     mainFrame->Center();
     mainFrame->Show();
     return true;
    }

--- wxBoxSizer/App.cpp
+++ wxBoxSizer/App.cpp
@@ -2,13 +2,11 @@
 #include "MainFrame.h"
 #include <wx/wx.h>
 
-//ref link:https://www.youtube.com/watch?v=cQalRGqRRp4&list=PLFk1_lkqT8MbVOcwEppCPfjGOGhLvcf9G&index=3&t=275s
-
 wxIMPLEMENT_APP(App);
 
-bool App::OnInit() {
+bool App::OnInit() 
+{
 	MainFrame* mainFrame = new MainFrame("C++ GUI");
-	mainFrame->SetClientSize(800, 600);
 	mainFrame->Center();
 	mainFrame->Show();
 	return true;
