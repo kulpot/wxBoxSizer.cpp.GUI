ebd404f10bb3ce9c4deb15dac877214dca5d277c
Kulpot <kulpotwahu@gmail.com>
2024-05-12 01:59:16 +0800

    MainFrame.cpp->
    wxSizerFlags flags = wxSizerFlags().Proportion(1).CenterVertical().Border(wxALL, 25);
    
     boxSizer->Add(button1, flags);
     boxSizer->Add(button2, flags);

--- wxBoxSizer/MainFrame.cpp
+++ wxBoxSizer/MainFrame.cpp
@@ -22,12 +22,15 @@ MainFrame::MainFrame(const wxString& title)
 	wxButton* button2 = new wxButton(this, wxID_ANY, "Button 2", wxDefaultPosition, wxSize(300, 100));
 
 	wxBoxSizer* boxSizer = new wxBoxSizer(wxHORIZONTAL);
+	wxSizerFlags flags = wxSizerFlags().Proportion(1).CenterVertical().Border(wxALL, 25);
 
 	//boxSizer->Add(button1, 1, wxLEFT | wxRIGHT, 25);
 	//boxSizer->Add(button1, 1, wxALIGN_CENTER | wxLEFT, 25);
-	boxSizer->Add(button1, wxSizerFlags().Proportion(1).CenterVertical().Border(wxALL, 25));
+	//boxSizer->Add(button1, wxSizerFlags().Proportion(1).CenterVertical().Border(wxALL, 25));
 	//boxSizer->Add(button1, 1, wxALL, 25);
-	boxSizer->Add(button2, 0);
+	//boxSizer->Add(button2, 0);
+	boxSizer->Add(button1, flags);
+	boxSizer->Add(button2, flags);
 
 	SetSizerAndFit(boxSizer);
 }
